4d6ffb47bd372d36d0ab7df33795eef7cdc5896b
Anna Kuzniak <Anna.Kuzniak@tomtom.com>
2018-09-20 13:14:45 +0200

    moved map to the component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+    script.src = process.env.PUBLIC_URL + "/sdk/tomtom.min.js";
+    document.body.appendChild(script);
+    script.async = true;
+    script.onload = function () {
+      window.tomtom.L.map('map', {
+        source: 'vector',
+        key: '<your-api-key>',
+        center: [37.769167, -122.478468],
+        basePath: '/sdk',
+        zoom: 15
+      });
+    }
+  }
+
   render() {
-    return (<div id="map"></div>);
+    return <div id = "map" ></div>
   }
 }
-const script = document.createElement('script');
 
-script.src = process.env.PUBLIC_URL + "/sdk/tomtom.min.js";
-script.async = false;
-
-script.onload = function() {
-  window.tomtom.L.map('map', {
-    source: 'vector',
-    key: '<your-api-key>',
-    center: [37.769167, -122.478468],
-    basePath: '/sdk',
-    zoom: 15
-  });
-}
-
-document.body.appendChild(script);
 export default App;
